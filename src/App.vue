5b80347421ecc642b484435c2ec48db80ede2606
Mavlon_Yusuf <mavlonjonyusupov1@gmail.com>
2024-04-08 23:48:16 +0500

    Table finished
    Table finished

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+    <TableProps />
+  </div>
+</template>
+
+<script setup>
+import TableProps from "./props/tableProp.vue";
+</script>
+
+<style scoped>
+.wrapper {
+  width: 1000px;
+  margin: 200px auto;
+}
+</style>
